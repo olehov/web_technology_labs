1c7107efe7371d06bf7a2d4eb74678d67a3cf2eb
Oleh Hrytsiv <g.r.i.t.s.i.v.o.l.e.g.95@gmail.com>
2022-11-28 17:19:02 +0000

    change urls

--- index.php
+++ index.php
@@ -3,19 +3,19 @@
         <meta charset="UTF-8">
     </head>
     <body>
-        <form action="/web_technology_labs/lab-11/lab_11.php">
+        <form action="/lab-11/lab_11.php">
             <input type="submit" value="Go to Lab 11"><br>
         </form>
-        <form action="/web_technology_labs/lab-12/lab_12.php">
+        <form action="/lab-12/lab_12.php">
             <input type="submit" value="Go to Lab 12"><br>
         </form>
-        <form action="/web_technology_labs/lab-13/lab_13.php">
+        <form action="/lab-13/lab_13.php">
             <input type="submit" value="Go to Lab 13"><br>
         </form>
-        <form action="/web_technology_labs/lab-14/lab_14.php">
+        <form action="/lab-14/lab_14.php">
             <input type="submit" value="Go to Lab 14"><br>
         </form>
-        <form action="/web_technology_labs/lab-15/index15.php">
+        <form action="/lab-15/index15.php">
             <input type="submit" value="Go to Lab 15"><br>
         </form>
         </form>
